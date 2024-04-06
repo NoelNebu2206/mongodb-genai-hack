ed8839c672d98983cbaaa2ab482185ad8ec6749f
Yogya Sharma <ys5250@nyu.edu>
2024-04-06 15:25:09 -0400

    Modal Updates

--- modal_image.py
+++ modal_image.py
@@ -1,3 +1,14 @@
-from modal import Image
+from modal import Image, Stub
 
-image = Image.debian_slim(python_version="3.11").pip_install("modal==0.62.21", "nomic").apt_install("git", "curl")
+image = Image.debian_slim(python_version="3.11").pip_install(
+    "modal==0.62.21",
+     "nomic",
+     "cohere",
+     "python-dotenv==1.0.0",
+     "pymongo==4.6.2",).apt_install("git", "curl")
+
+stub = Stub(
+    name="MongoTest",
+    image=image,
+    #secrets=[Secret.from_name("openai-secret")],
+)
