b8312c9e2bdb356708a967198d83a2ec3c0bd77c
Anubhav <anubhavghildiyal97@gmail.com>
2024-04-06 12:17:44 -0400

    AG: commented out non used code

--- backend.py
+++ backend.py
@@ -16,13 +16,14 @@
 # These are installed into a Debian Slim base image using the `pip_install` function.
 #
 # Because OpenAI's API is used, we also specify the `openai-secret` Modal Secret, which contains an OpenAI API key.
-#
+
 # A `docsearch` global variable is also declared to facilitate caching a slow operation in the code below.
 from pathlib import Path
 import modal
+from modal_image import image
 from modal import Image, Secret, Stub, web_endpoint, Volume
 print('hello')
-image = Image.debian_slim(python_version="3.11").pip_install("modal==0.62.21", "nomic").apt_install("git")
+#image = Image.debian_slim(python_version="3.11").pip_install("modal==0.62.21", "nomic").apt_install("git")
 # .run_commands(
 #     "cd /root",
 #     "ls ",
@@ -69,87 +70,87 @@ MODEL_DIR = "/model"
 # For more information on this, see [LangChain's "Question Answering" notebook](https://langchain.readthedocs.io/en/latest/use_cases/evaluation/question_answering.html).
 
 
-def retrieve_sources(sources_refs: str, texts: list[str]) -> list[str]:
-    """
-    Map back from the references given by the LLM's output to the original text parts.
-    """
-    clean_indices = [
-        r.replace("-pl", "").strip() for r in sources_refs.split(",")
-    ]
-    numeric_indices = (int(r) if r.isnumeric() else None for r in clean_indices)
-    return [
-        texts[i] if i is not None else "INVALID SOURCE" for i in numeric_indices
-    ]
+# def retrieve_sources(sources_refs: str, texts: list[str]) -> list[str]:
+#     """
+#     Map back from the references given by the LLM's output to the original text parts.
+#     """
+#     clean_indices = [
+#         r.replace("-pl", "").strip() for r in sources_refs.split(",")
+#     ]
+#     numeric_indices = (int(r) if r.isnumeric() else None for r in clean_indices)
+#     return [
+#         texts[i] if i is not None else "INVALID SOURCE" for i in numeric_indices
+#     ]
 
 
-def qanda_langchain(query: str) -> tuple[str, list[str]]:
-    from langchain.chains.qa_with_sources import load_qa_with_sources_chain
-    from langchain.embeddings.openai import OpenAIEmbeddings
-    from langchain.llms import OpenAI
-    from langchain.text_splitter import CharacterTextSplitter
-    from langchain.vectorstores.faiss import FAISS
+# def qanda_langchain(query: str) -> tuple[str, list[str]]:
+#     from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+#     from langchain.embeddings.openai import OpenAIEmbeddings
+#     from langchain.llms import OpenAI
+#     from langchain.text_splitter import CharacterTextSplitter
+#     from langchain.vectorstores.faiss import FAISS
 
-    # Support caching speech text on disk.
-    speech_file_path = Path("state-of-the-union.txt")
+#     # Support caching speech text on disk.
+#     speech_file_path = Path("state-of-the-union.txt")
 
-    if speech_file_path.exists():
-        state_of_the_union = speech_file_path.read_text()
-    else:
-        print("scraping the 2022 State of the Union speech")
-        state_of_the_union = scrape_state_of_the_union()
-        speech_file_path.write_text(state_of_the_union)
+#     if speech_file_path.exists():
+#         state_of_the_union = speech_file_path.read_text()
+#     else:
+#         print("scraping the 2022 State of the Union speech")
+#         state_of_the_union = scrape_state_of_the_union()
+#         speech_file_path.write_text(state_of_the_union)
 
-    # We cannot send the entire speech to the model because OpenAI's model
-    # has a maximum limit on input tokens. So we split up the speech
-    # into smaller chunks.
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    print("splitting speech into text chunks")
-    texts = text_splitter.split_text(state_of_the_union)
+#     # We cannot send the entire speech to the model because OpenAI's model
+#     # has a maximum limit on input tokens. So we split up the speech
+#     # into smaller chunks.
+#     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+#     print("splitting speech into text chunks")
+#     texts = text_splitter.split_text(state_of_the_union)
 
-    # Embedding-based query<->text similarity comparison is used to select
-    # a small subset of the speech text chunks.
-    # Generating the `docsearch` index is too slow to re-run on every request,
-    # so we do rudimentary caching using a global variable.
-    global docsearch
+#     # Embedding-based query<->text similarity comparison is used to select
+#     # a small subset of the speech text chunks.
+#     # Generating the `docsearch` index is too slow to re-run on every request,
+#     # so we do rudimentary caching using a global variable.
+#     global docsearch
 
-    if not docsearch:
-        # New OpenAI accounts have a very low rate-limit for their first 48 hrs.
-        # It's too low to embed even just this single Biden speech.
-        # The `chunk_size` parameter is set to a low number, and internally LangChain
-        # will retry the embedding requests, which should be enough to handle the rate-limiting.
-        #
-        # Ref: https://platform.openai.com/docs/guides/rate-limits/overview.
-        print("generating docsearch indexer")
-        docsearch = FAISS.from_texts(
-            texts,
-            OpenAIEmbeddings(chunk_size=5),
-            metadatas=[{"source": i} for i in range(len(texts))],
-        )
+#     if not docsearch:
+#         # New OpenAI accounts have a very low rate-limit for their first 48 hrs.
+#         # It's too low to embed even just this single Biden speech.
+#         # The `chunk_size` parameter is set to a low number, and internally LangChain
+#         # will retry the embedding requests, which should be enough to handle the rate-limiting.
+#         #
+#         # Ref: https://platform.openai.com/docs/guides/rate-limits/overview.
+#         print("generating docsearch indexer")
+#         docsearch = FAISS.from_texts(
+#             texts,
+#             OpenAIEmbeddings(chunk_size=5),
+#             metadatas=[{"source": i} for i in range(len(texts))],
+#         )
 
-    print("selecting text parts by similarity to query")
-    docs = docsearch.similarity_search(query)
+#     print("selecting text parts by similarity to query")
+#     docs = docsearch.similarity_search(query)
 
-    chain = load_qa_with_sources_chain(
-        OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0),
-        chain_type="stuff",
-    )
-    print("running query against Q&A chain.\n")
-    result = chain(
-        {"input_documents": docs, "question": query}, return_only_outputs=True
-    )
-    output: str = result["output_text"]
-    parts = output.split("SOURCES: ")
-    if len(parts) == 2:
-        answer, sources_refs = parts
-        sources = retrieve_sources(sources_refs, texts)
-    elif len(parts) == 1:
-        answer = parts[0]
-        sources = []
-    else:
-        raise RuntimeError(
-            f"Expected to receive an answer with a single 'SOURCES' block, got:\n{output}"
-        )
-    return answer.strip(), sources
+#     chain = load_qa_with_sources_chain(
+#         OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0),
+#         chain_type="stuff",
+#     )
+#     print("running query against Q&A chain.\n")
+#     result = chain(
+#         {"input_documents": docs, "question": query}, return_only_outputs=True
+#     )
+#     output: str = result["output_text"]
+#     parts = output.split("SOURCES: ")
+#     if len(parts) == 2:
+#         answer, sources_refs = parts
+#         sources = retrieve_sources(sources_refs, texts)
+#     elif len(parts) == 1:
+#         answer = parts[0]
+#         sources = []
+#     else:
+#         raise RuntimeError(
+#             f"Expected to receive an answer with a single 'SOURCES' block, got:\n{output}"
+#         )
+#     return answer.strip(), sources
 
 
 # ## Modal Functions
@@ -158,20 +159,20 @@ def qanda_langchain(query: str) -> tuple[str, list[str]]:
 # As said above, we're implementing a web endpoint, `web`, and a CLI command, `cli`.
 
 
-@stub.function()
-@web_endpoint(method="GET")
-def web(query: str, show_sources: bool = False):
-    answer, sources = qanda_langchain(query)
-    if show_sources:
-        return {
-            "answer": answer,
-            "sources": sources,
-        }
-    else:
-        return {
-            "answer": answer,
-        }
-print("tototo")
+# @stub.function()
+# @web_endpoint(method="GET")
+# def web(query: str, show_sources: bool = False):
+#     answer, sources = qanda_langchain(query)
+#     if show_sources:
+#         return {
+#             "answer": answer,
+#             "sources": sources,
+#         }
+#     else:
+#         return {
+#             "answer": answer,
+#         }
+# print("tototo")
 
 @stub.function(image=image,
                 volumes={'/data': volume},
@@ -181,28 +182,10 @@ def get_git_data(github_url:str):
     import subprocess
     import os
     import nomic
-    #import pdb
-    #import modal
     
-    print('stop[]')
-    # modal.interact()
-    # import IPython
-    # IPython.embed()
-    # image = image.run_commands(
-    #     "cd /root",
-    #     "git clone {github_url}",
-    #     "echo 'git cloned...' ",
-    # )
-    #modal.interact()
-    #breakpoint()
     print('AG: inside get_git_data')
-    #os.makedirs("/data", exist_ok=True)
-    #os.chdir("/data")
-    
-    # #print()
-    # # Run the git clone command
+    # Run the git clone command
     print('AG: starting git clone ')
-    subprocess.run(["echo", 'bobobo' ])
     subprocess.run(["git", "clone", github_url, 'github_data' ])
     git_content = []
     def read_file_content(directory):
@@ -225,21 +208,42 @@ def get_git_data(github_url:str):
 
     # Usage
     read_file_content('/data')
-    print(f"num of files read: {len(git_content)}")
+    print(f"nu4m of files read: {len(git_content)}")
     sample_file_data = """
         Once upon a time, in a quaint little town nestled between rolling hills and lush forests, there lived a friendly and adventurous dog named Max. Max was a mix of a Golden Retriever and a Border Collie, which made him both clever and affectionate.
         Max's days were filled with excitement and exploration. He would often roam the town, making friends with everyone he met. His favorite spot was the town square, where he would eagerly greet the townspeople and play with the local children.
         One day, while exploring the woods on the outskirts of town, Max stumbled upon a hidden path that led to a mysterious old house. Curiosity piqued, he bravely ventured inside, only to discover that the house was home to a family of friendly squirrels.
         The squirrels welcomed Max with open arms (or rather, open paws), and they quickly became the best of friends. Together, they would explore the woods, play games, and share stories late into the night.
         As the seasons changed, so did Max's adventures. In the winter, he would frolic in the snow with his squirrel friends, while in the summer, they would swim in the nearby creek and bask in the warm sun.
-        Through his adventures, Max taught the townspeople the importance of kindness, friendship, and the joy of exploring the world around them. And so, Max's story became a beloved tale in the town, inspiring everyone to embrace life with the same enthusiasm and curiosity as their furry friend.
-        """
+        Through his adventures, Max taught the townspeople the importance of kindness, friendship, and the joy of exploring the world around them. And so, Max's story became a beloved tale in the town, inspiring everyone to embrace life with the same enthusiasm and curiosity as their furry friend."""
+        
+    nomic_key = os.environ["NOMIC_API_KEY"]
+    print(nomic_key)
+    curl_command = [
+    "curl",
+    "https://api-atlas.nomic.ai/v1/embedding/text",
+    "-H",
+    "Authorization: Bearer {nomic_key}",
+    "-H",
+    "Content-Type: application/json",
+    "-d",
+    '{ "model": "nomic-embed-text-v1", "texts": ["sample_file_data"]}'
+    ]
     
-    # curl https://api-atlas.nomic.ai/v1/embedding/text \
-    # -H "Authorization: Bearer $NOMIC_API_KEY" \
-    # -H "Content-Type: application/json" \
-    # -d '{ "model": "nomic-embed-text-v1", "texts": ["The quick brown fox..."]}'
-    # #pdb.set_trace()
+    print('AG: starting curl')
+    # Run the curl command and capture the output
+    result = subprocess.run(curl_command, shell=True, stdout=subprocess.PIPE)
+
+    # Store the output in a variable
+    output = result.stdout.decode('utf-8')
+
+    # Print the output
+    print(output)
+    print(type(output))
+    print(len(output))
+    # Run the curl command
+    #subprocess.run(curl_command, shell=True)
+    
     volume.commit()
     
     print('AG: git clone done')
